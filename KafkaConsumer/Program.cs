2e7a3327616787102caf22dbff7c5bc738442da1
Matija Cvetan <matija.cvetan@t.ht.hr>
2023-05-22 14:07:53 +0200

    Add project files.

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    BootstrapServers = "localhost:9092",
+    GroupId = "test-consumer-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+{
+    consumer.Subscribe("test_topic");
+
+    Console.WriteLine("Consuming messages...");
+
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+
+        Console.WriteLine($"Received message: {consumeResult.Message.Value} | " +
+            $"Topic: {consumeResult.Topic} | " +
+            $"Partition: {consumeResult.Partition} | " +
+            $"Offset: {consumeResult.Offset}");
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+using (var producer = new ProducerBuilder<Null, string>(config).Build())
+{
+    string message;
+    
+    do 
+    {
+        Console.WriteLine("Enter a message:");
+        message = Console.ReadLine();
+
+        var kafkaMessage = new Message<Null, string>
+        {
+            Value = message
+        };
+
+        producer.Produce("test_topic", kafkaMessage, deliveryReport =>
+        {
+            if (deliveryReport.Error.Reason is not "Success")
+            {
+                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+            }
+            else
+            {
+                Console.WriteLine($"Message delivered to '{deliveryReport.TopicPartitionOffset}'");
+            }
+        });
+    } while (message != null);
+    
+}
